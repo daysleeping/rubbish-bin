dfa7e94c39ab33b49430e3ee8dda8e442e39cf2f
daysleeping <101317245+daysleeping@users.noreply.github.com>
2022-05-17 17:20:05 +0800

    Create english_vocab.py

---
+++ english_vocab.py
@@ -0,0 +1,37 @@
+answer = ['cultural traditions','beauty trends','plump','attractive','desirable','poverty','prosperity','afford','popularity','multicultural','evolve','tattooing','expand','a fashionable trend','a sign of beauty','body piercings','status and wealth','doubtless to say','beliefs','define','in danger','preserve','landmarks','future generations','safeguard','unique','identity','historical buildings','impact','rural villages','entire','united people','symbolic','urban areas','historical importance','rituals','worship','ancestor','convenient','adapt'] #Put your words in this list
+user_answer = []
+a = answer.copy
+
+print(len(answer))
+
+def get_user():
+    a = 1
+    while a != len(answer) + 1:
+        user_answer.append(input(f'{a} : '))
+        a += 1
+
+def check_without_order(user_answer,answer):
+    a = 0
+    b = 0
+    correct = []
+    while a != len(user_answer):
+        while b != len(answer):
+            if user_answer[a] == None:
+                break
+            if user_answer[a] == answer[b]:
+                print(f'your answer {a} : {user_answer[a]} == correct answer {b} : {answer[b]}')
+                correct.append(user_answer[a])
+                answer.pop(b)
+                break
+            else:
+                print(f'your answer {a} : {user_answer[a]} != correct answer {b} : {answer[b]}')
+                b += 1
+        a += 1
+        b = 0
+    return [answer,correct]
+
+
+get_user()
+list = check_without_order(user_answer,answer)
+
+print(f'Not answered : {list[0]} \nAnswered successfully : {list[1]} \nYour answer : {user_answer} \nCorrect answer : {a}')
